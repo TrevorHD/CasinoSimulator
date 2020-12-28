f6e9ba9c95a26528f8b679e5e7e5b50a6140eb85
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2020-12-27 18:00:13 -0600

    File upload 2
    Uploaded code for the Blackjack game; very much a work in progress.

---
+++ Blackjack.py
@@ -0,0 +1,242 @@
+# ----- Import packages -----------------------------------------------------------------------------------------------------
+
+# Import packages
+import pandas as pd
+import numpy as np
+import random
+
+# Define function to clear text from console
+import os
+def clear(): os.system('cls') #on Windows System
+clear()
+
+
+
+
+
+# ----- Pre-run setup -------------------------------------------------------------------------------------------------------
+
+# Create a double-deck of cards; will add in aces once base code works
+deck = list(map(str, range(2, 11, 1)))*8 + ["J", "Q", "K"]*8
+
+# Define function to shuffle double-deck
+def shuffle():
+    shuffleCards = random.sample(deck, len(deck))
+    return shuffleCards
+
+# Define function to check sum of any given hand
+def handSum(hand):
+    try:
+        hSum = sum(list(map(int, hand)))
+    except ValueError:
+        if "J" in hand or "K" in hand or "Q" in hand:
+            hSum = sum(list(map(int, ["10" if x == "J" or x == "Q" or x == "K" else x for x in hand])))
+    return hSum  
+
+      
+    
+# Set initial values
+balance = 1000
+betStage = 1
+inputError = False
+cheatMessage = 0
+cheatL = False
+cheatS = False
+decisionNumP = 1
+decisionNumD = 1
+
+
+
+
+# ----- Run game ------------------------------------------------------------------------------------------------------------
+
+# Start running the game
+while betStage >= 1:
+    
+    # ----- Step 1: Select Wager ----------------------------------------------
+    
+    # Choose amount to bet
+    while betStage == 1:
+        clear()
+        print("-"*5, "STEP 1: Select Wager", "-"*88, "\n")
+        if inputError == 1:
+            print("You do not have enough credits. Please enter a smaller amount.", "\n")
+        elif inputError == 2:
+            print("Please enter a number greater than zero.", "\n")
+        elif inputError == 3:
+            print("Invalid input, try again.", "\n")
+        elif cheatMessage == 1:
+            print("2000 credits added to your balance. Gambling on credit seems a bit irresponsible, no?", "\n")
+        elif cheatMessage == 2:
+            print("1000 credits added to your balance. Who needs savings anyway?", "\n")
+        elif cheatMessage == 3:
+            print("You've already used this cheat code!", "\n")
+        else:
+            print("\n")
+        print("You have", balance, "credits remaining in your account.")
+        inputError = False
+        cheatMessage = 0
+        betAmount = input("Please enter a whole amount to wager: ")
+        if betAmount == "b":
+            betStage = betStage - 1
+        elif betAmount in ["Loan", "loan"]:
+            if cheatL == True:
+                cheatMessage = 3
+            else:
+                balance = balance + 2000
+                cheatMessage = 1
+                cheatL = True
+        elif betAmount in ["Savings Account", "Savings account", "savings account", "savings Account", 
+                           "SavingsAccount", "Savingsaccount", "savingsaccount", "savingsAccount"]:
+            if cheatS == True:
+                cheatMessage = 3
+            else:
+                balance = balance + 1000
+                cheatMessage = 2
+                cheatS = True
+        else:
+            try:
+                if int(betAmount) > balance:
+                    inputError = 1
+                elif int(betAmount) <= 0:
+                    inputError = 2
+                else:
+                    betStage = betStage + 0.5
+            except ValueError:
+                inputError = 3
+      
+    # ----- Step 1.5: Initial dealing -----------------------------------------
+    
+    # Deal two cards to dealer and player
+    while betStage == 1.5:
+        cardQueue = shuffle()
+        cardDealer = []
+        cardPlayer = []          
+        cardDealer.append(cardQueue.pop())
+        cardPlayer.append(cardQueue.pop())
+        cardDealer.append(cardQueue.pop())
+        cardPlayer.append(cardQueue.pop())
+        betStage = betStage + 0.5
+        
+    # ----- Step 2: Player decisions ------------------------------------------
+    
+    # Allow player to take turn
+    while betStage == 2:
+        clear()
+        print("-"*5, "STEP 2: Player Turns", "-"*88, "\n")
+        if inputError == True:
+            print("Invalid input, try again.", "\n")
+        else:
+            print("\n")
+        print("Dealer totals", handSum(cardDealer), "with cards: ?", cardDealer[1])
+        print("Player totals", handSum(cardPlayer), "with cards:", *cardPlayer)
+        print("Your current bet is", betAmount, "credits.", "\n")
+        if decisionNumP == 1:
+            print("You were given card(s)", *cardPlayer, "\n")
+        else:
+            print("You were given card(s)", cardPlayer[len(cardPlayer) - 1], "\n")
+        inputError = False
+        if handSum(cardPlayer) > 21:
+            decision = input("You busted, now it is the dealer's turn. Hit enter to continue.")
+            betStage = betStage + 1
+        else:
+            # note: will put in option for split later
+            if(decisionNumP == 1):
+                decision = input("Your move. Hit, stand, double down, or surrender: ")
+            else:
+                decision = input("You're still in. Hit or stand: ")
+            if decision in ["Hit", "hit"]:
+                cardPlayer.append(cardQueue.pop())
+            elif decision in ["Stand", "stand"]:
+                decision = input("You stand and your turn is over; now it is the dealer's turn. Hit enter to continue.")
+                betStage = betStage + 1
+            elif decision in ["Double Down", "Double down", "double down", "double Down",
+                              "Doubledown", "DoubleDown", "doubledown", "doubleDown"] and decisionNumP == 1:
+                betAmount = int(betAmount)*2
+                cardPlayer.append(cardQueue.pop())
+                betStage = betStage + 1
+            elif decision in ["Surrender", "surrender"] and decisionNumP == 1:
+                betAmount = int(betAmount)*0.5
+                decision = input("You surrender and your turn is over; now it is the dealer's turn. Hit enter to continue.")
+                betStage = betStage + 1
+            else:
+                inputError = True
+            if inputError == False:
+                decisionNumP = decisionNumP + 1
+    
+    # ----- Step 3: Dealer decisions ------------------------------------------
+    
+    # Dealer takes turn
+    while betStage == 3:
+        clear()
+        print("-"*5, "STEP 3: Dealer Turns", "-"*88, "\n")
+        if inputError == True:
+            print("Invalid input, try again.", "\n")
+        else:
+            print("\n")
+        print("Your current bet is", betAmount)
+        if decisionNumD == 1:
+            print("Dealer was given card(s)", *cardDealer, "\n")
+        else:
+            print("Dealer was given card(s)", cardDealer[len(cardDealer) - 1], "\n")
+        print("Dealer has cards", *cardDealer, "with total of", handSum(cardDealer))
+        print("You have cards", *cardPlayer, "with total of", handSum(cardPlayer))
+        if handSum(cardDealer) > 21:
+            decision = input("Dealer busted. Press enter to continue.")
+            betStage = betStage + 1
+        elif handSum(cardDealer) <= 16:
+            if decisionNumD == 1:
+                decision = input("Dealer reveals hole card. Press enter to continue.")
+            else:
+                cardDealer.append(cardQueue.pop())
+                decision = input("Dealer hits. Press enter to continue.")
+        else:
+            betStage = betStage + 1
+            decision = input("Dealer stands. Press enter to continue.")
+        decisionNumD = decisionNumD + 1
+        
+    # ----- Step 4: Results ---------------------------------------------------
+    
+    # Dealer takes turn
+    while betStage == 4:
+        clear()
+        print("-"*5, "STEP 4: Results", "-"*93, "\n")
+        if inputError == True:
+            print("Invalid input, try again.", "\n")
+        else:
+            print("\n")
+        if handSum(cardDealer) > 21 and handSum(cardPlayer) > 21:
+            print("Player busts, dealer busts.")
+            print("Loss of", betAmount)
+            balance = balance - int(betAmount)
+        elif handSum(cardDealer) > 21 and handSum(cardPlayer) <= 21:
+            print("Player wins, dealer busts.")
+            print("Payout of", betAmount)
+            balance = balance + int(betAmount)
+        elif handSum(cardDealer) <= 21 and handSum(cardPlayer) > 21:
+            print("Player busts, dealer wins.")
+            print("Loss of", betAmount)
+            balance = balance - int(betAmount)
+        elif handSum(cardDealer) <= 21 and handSum(cardPlayer) <= 21 and handSum(cardDealer) > handSum(cardPlayer):
+            print("Player loses, dealer wins.")
+            print("Loss of", betAmount)
+            balance = balance - int(betAmount)
+        elif handSum(cardDealer) <= 21 and handSum(cardPlayer) <= 21 and handSum(cardDealer) < handSum(cardPlayer):
+            print("Player wins, dealer loses.")
+            print("Payout of", betAmount)
+            balance = balance + int(betAmount)
+        elif handSum(cardDealer) <= 21 and handSum(cardPlayer) <= 21 and handSum(cardDealer) == handSum(cardPlayer):
+            print("Push; bet returned to player.")
+        input("Press enter to continue.")
+        decisionNumP = 1
+        decisionNumD = 1
+        betStage = 1
+        
+        
+        
+        
+        
+        
+    # Rules: dealer must draw on 16 and stand on 17, blackjack pays 2:1, early surrender only, double down
+    #        available on soft count (even with aces) on initial hand only
+    
